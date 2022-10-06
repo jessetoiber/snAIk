847cef98f9e7bc11186917427fbb43dd8d8aa2ae
Jamie Sangerman <jtsangerman@email.arizona.edu>
2022-10-05 22:07:22 -0700

    cleanup

--- src/game/game_driver.py
+++ src/game/game_driver.py
@@ -1,15 +1,9 @@
-from src.game.game_models import *
 from src.game.game_engine import *
-from src.game.game_next_move_decision_helpers import *
 
-# game constants
 __tick_rate_seconds = .001
 
 def drive_game():
     game_engine = GameEngine()
 
-    # Run until the user asks to quit
     while game_engine.running:
         game_engine.execute_game_tick(__tick_rate_seconds)
-
-
--- src/start_game.py
+++ src/start_game.py
@@ -1,3 +1,5 @@
+# wrapper file for game start logic -- currently doesn't do much, is verbose
+
 from src.game.game_driver import drive_game
 
 def start_game():
